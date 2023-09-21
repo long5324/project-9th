f8730448fe7e88dccbe737fb30092ed7aa3811db
longhaycode <longbo2k3@gmail.com>
2023-09-21 23:56:13 +0700

    end day 2

--- ennemy scrip/animation.cs
+++ ennemy scrip/animation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -6,22 +7,27 @@ using UnityEngine.InputSystem;
 
 public class animation : MonoBehaviour
 {
-    bool is_run;
+    movement movement_enemy;
     Animator animator_enemy;
     private void Start()
     {
+        movement_enemy = GetComponentInParent<movement>();
         animator_enemy = GetComponent<Animator>();
     }
-    private void Update()
+    private void LateUpdate()
     {
-        if(is_run)
-        animator_enemy.SetTrigger("isRun");
+        animator_enemy.SetFloat("velocityy", movement_enemy.e_rigd.velocity.y);
+        animator_enemy.SetFloat("velocityx", Math.Abs(movement_enemy.e_rigd.velocity.x));
+        if (movement_enemy.groundCheck()&&animator_enemy.GetBool("buttun_jump")==true&& movement_enemy.e_rigd.velocity.y <0.1 )
+        {
+            animator_enemy.SetBool("buttun_jump", false);
+        }
     }
-    public void Move(InputAction.CallbackContext context)
+    public void Jum_animator(InputAction.CallbackContext context)
     {
-        if (context.action.ReadValue<Vector2>().x != 0)
-        is_run = true;
-        else
-        is_run = false;
+        if (context.action.ReadValue <float>()== 1)
+        {
+            animator_enemy.SetBool("buttun_jump", true);
+        }
     }
 }
--- ennemy scrip/movement.cs
+++ ennemy scrip/movement.cs
@@ -5,36 +5,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class movement : MonoBehaviour
 {
-    [SerializeField] float speed_movement = 4, focre_jum = 10;
-    Rigidbody2D e_rigd;
-    float Horizontal;
-    bool facelook=true;
+    [SerializeField] LayerMask ground_check;
+    [SerializeField] Vector2 size_boxcast;
+    [SerializeField] float speed_movement = 4, focre_jum = 10, casDistance, time_wait_jum = 0.3f;
+   public  Rigidbody2D e_rigd { get; set; }
+    public float Horizontal { get; set; }
+    bool facelook=true,can_jum=true;
     void Start()
     {
-        
         e_rigd = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
+        e_rigd.velocity = new Vector2(Horizontal * speed_movement, e_rigd.velocity.y);
         if (Horizontal > 0 && !facelook)
             flip();
         else if (Horizontal < 0 && facelook)
             flip();
     }
-    void FixedUpdate()
-    {
-        e_rigd.velocity = new Vector2(Horizontal*speed_movement, e_rigd.velocity.y);
-    }
     // Update is called once per frame
     public void Move(InputAction.CallbackContext context)
     {
+        
         Horizontal = context.action.ReadValue<Vector2>().x;
     }
     public void Jum(InputAction.CallbackContext context)
     {
-        if (context.action.ReadValue<bool>())
+        if (context.action.ReadValue<float>()==1&& groundCheck() == true&& can_jum == true)
         {
-            e_rigd.velocity = new Vector2(e_rigd.velocity.x,10f);
+            e_rigd.velocity = new Vector2(e_rigd.velocity.x,focre_jum);
+            can_jum = false;
+            StartCoroutine(waitjum());
         }
     }
     private void flip()
@@ -43,5 +44,22 @@ public class movement : MonoBehaviour
         Scale.x *= -1;
         transform.localScale = Scale;
         facelook = !facelook;
+    }
+    public bool groundCheck()
+    {
+
+        if (Physics2D.BoxCast(transform.position, size_boxcast, 0f, Vector2.down, casDistance, ground_check))
+        return true;
+        else return false;
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position-transform.up*casDistance,size_boxcast);
+    }
+    private IEnumerator waitjum() {
+       yield return new WaitForSeconds(time_wait_jum);
+       
+        can_jum = true;
     }
 }
